f6e7a04752ed7ac9ec80d78b80f6cff792986b46
Helen Escanio <helenescanio@gmail.com>
2021-04-14 14:50:07 -0400

    Add project files.

---
+++ Models/Book.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApi.Models
+{
+    public class Book
+    {
+        [Required]
+        public int id { get; set; }
+        [Required]
+        public string title { get; set; }
+        public string description { get; set; }
+        public decimal pageCount { get; set; }
+        [Required]
+        public string excerpt { get; set; }
+        public string publishDate { get; set; }
+
+    }
+}
